2237755901731950094b7ba6b94d316f9c8e852a
Keitaro Kotani <kei.kotani77@gmail.com>
2017-05-30 10:49:52 +0900

    プロジェクトをコミット

---
+++ src/GameServer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : GameServer.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+
+int main() {
+
+	printf("test\n");
+	return 0;
+}
